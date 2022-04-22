387e6df7c000cfeac9c5ebc9fbd1766c060dc57a
manoel <jmsneto120@gmail.com>
2022-04-22 13:12:23 -0400

    create invalid user error

--- src/modules/Financial/useCases/CreateWallet/errors/InvalidWalletError.ts
+++
@@ -1,8 +0,0 @@
-import { UseCaseError } from '@core/domain/errors/UseCaseError'
-
-export class InvalidWalletError extends Error implements UseCaseError {
-  constructor() {
-    super(`Wallet data is invalid.`)
-    this.name = 'InvalidWalletError'
-  }
-}
