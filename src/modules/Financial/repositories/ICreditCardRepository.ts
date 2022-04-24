f35059f21ac36af116da9742c431fe353a05eed4
manoel <jmsneto120@gmail.com>
2022-04-24 12:12:49 -0400

    create crud and interface repository

---
+++ src/modules/Financial/repositories/ICreditCardRepository.ts
@@ -0,0 +1,8 @@
+import { CreditCard } from './../domain/creditCard/creditCard';
+
+export interface ICreditCardRepository {
+  create(creditCard: CreditCard): Promise<void>
+  delete(id: string): Promise<void>
+  save(creditCard: CreditCard): Promise<void>
+  findById(id: string): Promise<CreditCard>
+}
---
+++ src/modules/Financial/repositories/prisma/PrismaCreditCardRepository.ts
@@ -0,0 +1,48 @@
+import { prisma } from '@infra/prisma/client';
+import { CreditCard } from '@modules/Financial/domain/creditCard/creditCard';
+import { CreditCardMapper } from '@modules/Financial/mappers/CreditCardMapper';
+
+import { ICreditCardRepository } from './../ICreditCardRepository';
+
+export class PrismaCreditCardRepository implements ICreditCardRepository{
+
+  async create(creditCard: CreditCard): Promise<void> {
+    const data = CreditCardMapper.toPersistence(creditCard)
+
+    await prisma.creditCard.create({
+     data,
+    })
+  }
+
+  async delete(id: string): Promise<void> {
+    await prisma.creditCard.delete({
+      where: {
+        id
+      }
+    })
+  }
+
+  async save(creditCard: CreditCard): Promise<void> {
+    const data = CreditCardMapper.toPersistence(creditCard)
+
+    await prisma.creditCard.update({
+      where: {
+        id: creditCard.id
+      },
+      data
+    })
+  }
+
+  async findById(id: string): Promise<CreditCard> {
+    const creditCard = await prisma.creditCard.findUnique({
+      where: {
+        id
+      }
+    })
+
+    if(!creditCard) return null
+
+    return CreditCardMapper.toDomain(creditCard)
+  }
+
+}
